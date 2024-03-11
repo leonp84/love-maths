b567bc469788bd8f1524a1888ab60790035111a5
Leon Potgieter <hexvallei@gmail.com>
2024-03-11 16:48:31 +0000

    finished addition game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type" === "submit")) {
-                alert("You clicked Submit!")
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -25,18 +25,42 @@ function runGame(gameType) {
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
-        alert(`Uknown game type: ${gameType}`);
+        alert(`Unknown game type: ${gameType}`);
         throw `Uknown game type: ${gameType}. Aborting;`
     }
 }
 
-
+/**
+ * Checks answer and returns answer + game type
+ */
 function checkAnswer() {
-    
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Nice :)");
+    } else {
+        alert(`Nope, correct answer is not ${userAnswer}, but actually ${calculatedAnswer[0]}.`)
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
+/**
+ * Gets values from DOM and returns correct answer
+ */
 function calculateCorrectAnswer() {
-    
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]
+    } else {
+        alert(`No go... not with ${operator}. Fix it!`);
+        throw `No go... not with ${operator}. Aborting`;
+    }
 }
 
 function incrementScore() {
